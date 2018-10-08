55858c97756d20dfa5336d6f6666bfeb38237345
Fabian Roth <Fabian.V.Roth@gmail.com>
2018-10-08 15:08:49 +0200

    Updates test code

---
+++ testing.py
@@ -0,0 +1,83 @@
+import unittest
+import xml_parser
+import card_generator
+
+
+class ParserTests(unittest.TestCase):
+
+    def test_link_removal(self):
+        class_under_test = xml_parser
+        test_string = "Text<a href=bla></a>moreText"
+        expected_string = "Text <i>link</i> moreText"
+        actual_string = class_under_test.remove_link_tags(test_string)
+
+        self.assertEqual(expected_string, actual_string)
+
+    def test_complex_link_removal(self):
+        test_string = "Text<a href=bla></a>moreText<a href=www.google.com></a>"
+        expected_string = "Text <i>link</i> moreText <i>link</i> "
+        actual_string = xml_parser.remove_link_tags(test_string)
+
+        self.assertEqual(expected_string, actual_string)
+
+    def test_link_removal_with_alternative_tag(self):
+        test_string = "Text <a name=noHyperLink></a> moreText<a href=importantTag></a>"
+        expected_string = "Text <a name=noHyperLink></a> moreText <i>link</i> "
+        actual_string = xml_parser.remove_link_tags(test_string)
+
+        self.assertEqual(expected_string, actual_string)
+
+    def test_line_removal(self):
+        test_string = "A text \n with \n too \n many \n new \n lines"
+        expected_result = "A text \n with \n too \n many "
+        actual_string = xml_parser.remove_excessive_new_lines(test_string)
+
+        self.assertEqual(actual_string, expected_result)
+
+
+class GeneratorTests(unittest.TestCase):
+
+    def test_colors(self):
+        class_under_test = card_generator.Generator()
+        class_under_test.load_colors()
+        actual_colors = class_under_test.colors
+
+        self.assertEqual(len(actual_colors), 10)
+
+    def test_rank_style_choice(self):
+        class_under_test = card_generator.Generator()
+        short_rank = "774"
+        assignee = "Meyer, Max"
+        first_line_style = "A first line style"
+
+        rank_style = class_under_test.get_rank_style(short_rank, assignee, first_line_style)
+        self.assertEqual(rank_style, first_line_style)
+
+        # repeat with unassigned task, should not matter with a short rank
+        rank_style = class_under_test.get_rank_style(short_rank, class_under_test.UNASSIGNED,
+                                                     first_line_style)
+        self.assertEqual(rank_style, first_line_style)
+
+    def test_rank_style_choice_with_lexo_rank(self):
+        class_under_test = card_generator.Generator()
+        assignee = "Meyer, Max"
+        lexo_rank = "0|hzzzz7:"
+        first_line_style = "firstLine"
+
+        rank_style = class_under_test.get_rank_style(lexo_rank, assignee, first_line_style)
+        self.assertEqual(rank_style, class_under_test.LARGE_RANK_STYLE)
+
+        # repeat with unassigned task
+        rank_style = class_under_test.get_rank_style(lexo_rank, class_under_test.UNASSIGNED,
+                                                     first_line_style)
+        self.assertEqual(rank_style, class_under_test.LARGE_RANK_STYLE_UNASSIGNED)
+
+    def test_color_choice_for_unassigned(self):
+        class_under_test = card_generator.Generator()
+        card_color, line_style = class_under_test.get_first_line_style(class_under_test.UNASSIGNED)
+        self.assertEqual(card_color, class_under_test.ROYAL_BLUE)
+        self.assertEqual(line_style, class_under_test.FIRST_LINE_UNASSIGNED)
+
+
+if __name__ == '__main__':
+    unittest.main()
