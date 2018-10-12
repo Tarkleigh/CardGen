a73ee52b795f58bf2ff97f850903cef0e0b75966
Fabian Roth <Fabian.V.Roth@gmail.com>
2018-10-12 16:02:22 +0200

    Adds docstrings to xml_parser

--- xml_parser.py
+++ xml_parser.py
@@ -1,3 +1,6 @@
+"""Reads a given XML and passed its values to card_generator.py to create a pdf of cards.
+Once the generation is complete, the created pdf file will be opened automatically with the
+standard programme for this file type"""
 import html
 import os
 import subprocess
@@ -11,6 +14,7 @@ import card_generator
 
 
 def get_entries_from_xml(xml_tree):
+    """Convert xml data to a list of dictionaries, with one entry for each backlog item."""
     entries = []
 
     for item in xml_tree.iter("item"):
@@ -33,6 +37,7 @@ def get_entries_from_xml(xml_tree):
 
 
 def extract_description(item):
+    """Extract description of item"""
     description = extract_value(item, "description")
     description = remove_excessive_new_lines(description)
     description = trim_description(description)
@@ -41,12 +46,14 @@ def extract_description(item):
 
 
 def trim_description(description):
+    """Trim description, if it is longer than 160 characters."""
     if len(description) > 160:
         description = description[0:161] + '...'
     return description
 
 
 def extract_value(item, key):
+    """Get the value for the given key or return an empty string if no value is found."""
     value = item.find(key).text
     if value is None:
         value = ""
@@ -56,18 +63,21 @@ def extract_value(item, key):
 
 
 def sanitize_value(value):
+    """Remove values that would break card generation."""
     value = remove_link_tags(value)
     value = check_and_escape(value)
     return value
 
 
 def initialize_tkinter():
+    """Load tkinter for file choosing dialog."""
     import tkinter as tk
     root = tk.Tk()
     root.withdraw()
 
 
 def check_and_escape(string):
+    """Check if string can be used by Reportlab and escape if needed."""
     # reportLab can't handle gifs (they cause a hard crash), so we try to create a Paragraph
     # and escape if this doesn't work
     try:
@@ -78,6 +88,7 @@ def check_and_escape(string):
 
 
 def remove_excessive_new_lines(string):
+    """Change string so there are at most four line breaks."""
     newlines = 0
     index = string.find('\n')
 
@@ -93,6 +104,7 @@ def remove_excessive_new_lines(string):
 
 
 def remove_link_tags(string):
+    """Remove link tags in string."""
     tag_start_index = string.find("<a href")
     while tag_start_index != -1:
         end_index = string.find("</a>", tag_start_index)
@@ -102,6 +114,7 @@ def remove_link_tags(string):
 
 
 def extract_rank_from_custom_fields(item):
+    """Get the rank value from the custom fields."""
     rank = "0"
     for custom_field in item.iter("customfield"):
         if custom_field.find("customfieldname").text == "Rank":
@@ -113,6 +126,7 @@ def extract_rank_from_custom_fields(item):
 
 
 def open_output_file(file_path):
+    """Open created PDF file."""
     if sys.platform == "win32":
         os.startfile(file_path)
     elif sys.platform == "darwin":  # mac
@@ -122,6 +136,7 @@ def open_output_file(file_path):
 
 
 def get_file_paths():
+    """Get input and output paths needed for generation."""
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
     else:
@@ -134,6 +149,7 @@ def get_file_paths():
 
 
 def main():
+    """Start card generation."""
     file_path, output_path = get_file_paths()
     xml_tree = elementTree.parse(file_path)
     entries = get_entries_from_xml(xml_tree)
